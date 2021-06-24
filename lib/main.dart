6e00beca6905bd49fbcc870c86ae74314aa7a2e4
Sudip Thapa <sthapa@deerhold.org>
2021-06-24 14:21:52 +0545

    image demo complete

--- lib/main.dart
+++ lib/main.dart
@@ -17,17 +17,28 @@ class MyApp extends StatelessWidget {
       home: Container(
         padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
         decoration: BoxDecoration(
-          color: Colors.green
+          color: Colors.white
         ),
-        child: Text(
-          "Hello World",
-          style: TextStyle(
-            fontSize: 18,
-            color: Colors.black,
-            fontWeight: FontWeight.normal,
-            decoration: TextDecoration.none
-          ),
-        ),
+        child: Column(
+          children: [
+            Text('dfsahjkfldsakhfdoajhohdafdiosa', style: TextStyle(
+              fontFamily: 'Schyler',
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w600
+            ),),
+            Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                image: DecorationImage(
+                  image: NetworkImage('https://images.unsplash.com/photo-1532074205216-d0e1f4b87368?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d29tYW4lMjBwcm9maWxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'),
+                  fit: BoxFit.cover
+                )
+              ),
+              )
+          ],
+        )
       ),
     );
   }
