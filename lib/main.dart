0dfc13c239ec25fb2a893fcff048c47b750319e0
Sudip Thapa <sthapa@deerhold.org>
2021-06-24 13:50:15 +0545

    layout explained

--- lib/main.dart
+++ lib/main.dart
@@ -15,20 +15,60 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Container(
-        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-        decoration: BoxDecoration(
-          color: Colors.green
+        color: Colors.white,
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.only(top: 30),
+              width: 360,
+              height: 360,
+              color: Colors.black12,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.add),
+                      Icon(Icons.add_a_photo)
+                  ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(Icons.person),
+                      Text("Heading"),
+                      Divider(color: Colors.red,),
+                      Text("Subtitle"),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Test"),
+                          Text("Test"),
+                        ],
+                      )
+                    ]
+                  )
+
+                ]
+              ),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Text("8000"),
+                      Icon(Icons.money),
+                    ],
+                  ),
+                  Text("test")
+                ],
+              ),
+            )
+          ]
         ),
-        child: Text(
-          "Hello World",
-          style: TextStyle(
-            fontSize: 18,
-            color: Colors.black,
-            fontWeight: FontWeight.normal,
-            decoration: TextDecoration.none
-          ),
-        ),
-      ),
+      )
     );
   }
 }
