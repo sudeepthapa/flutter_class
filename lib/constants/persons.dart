b608bf9e08d73e5d777f09876eda78ad6d30ef6a
Sudip Thapa <sthapa@deerhold.org>
2021-07-07 13:46:18 +0545

    horizontal and vertical list item

---
+++ lib/constants/persons.dart
@@ -0,0 +1,50 @@
+List<Map<String, dynamic>> persons = [
+  {
+    'name': 'Alex',
+    'age': 22,
+    'occupation': 'Farmer',
+    'gender': 'Male'
+  },
+  {
+    'name': 'Ram',
+    'age': 19,
+    'occupation': 'Engineer',
+    'gender': 'Male'
+  },
+  {
+    'name': 'Sita',
+    'age': 24,
+    'occupation': 'Farmer',
+    'gender': 'Female'
+  },
+  {
+    'name': 'Hari',
+    'age': 33,
+    'occupation': 'Doctor',
+    'gender': 'Male'
+  },
+  {
+    'name': 'Alex',
+    'age': 22,
+    'occupation': 'Farmer',
+    'gender': 'Male'
+  },
+  {
+    'name': 'Ram',
+    'age': 19,
+    'occupation': 'Engineer',
+    'gender': 'Male'
+  },
+  {
+    'name': 'Sita',
+    'age': 24,
+    'occupation': 'Farmer',
+    'gender': 'Female'
+  },
+  {
+    'name': 'Hari',
+    'age': 33,
+    'occupation': 'Doctor',
+    'gender': 'Male'
+  },
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_class/constants/persons.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(
-        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-        decoration: BoxDecoration(
-          color: Colors.green
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-        child: Text(
-          "Hello World",
-          style: TextStyle(
-            fontSize: 18,
-            color: Colors.black,
-            fontWeight: FontWeight.normal,
-            decoration: TextDecoration.none
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('List Demo'),
           ),
-        ),
-      ),
+          body: Column(
+            children: [
+              Container(
+                height: 100,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    ...getHorizantalListItems()
+                  ],
+                ),
+              ),
+              Expanded(child: ListView(
+                children: [
+                  ...prepareListItems()
+                ]
+              ))
+            ],
+          ),
+        ));
+  }
+
+  List<Widget> prepareListItems() {
+    return persons.map((person) => getListItem(person)).toList();
+  }
+
+  Widget getListItem(person) {
+    return ListTile(
+      title: Text(person['name']),
+      subtitle: Text(person['occupation']),
+      leading: CircleAvatar(child: Text(person['age'].toString())),
+      trailing: Text(person['gender']),
     );
+  }
+
+  List<Widget> getHorizantalListItems() {
+    return [
+      Container(
+        width: 100,
+        height: 100,
+        color: Colors.redAccent,
+      ),
+      Container(
+        width: 100,
+        height: 100,
+        color: Colors.greenAccent,
+      ),
+      Container(
+        width: 100,
+        height: 100,
+        color: Colors.black12,
+      ),
+      Container(
+        width: 100,
+        height: 100,
+        color: Colors.blueAccent,
+      ),
+      Container(
+        width: 100,
+        height: 100,
+        color: Colors.yellow,
+      ),
+      Container(
+        width: 100,
+        height: 100,
+        color: Colors.grey,
+      ),
+      Container(
+        width: 100,
+        height: 100,
+        color: Colors.green,
+      ),
+    ];
   }
 }
