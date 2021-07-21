d961143776fd4731b3c4fc4c89127fcc88750abb
Sudip Thapa <sthapa@deerhold.org>
2021-07-21 14:10:47 +0545

    image picked from camera and gallery

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,45 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  XFile? pickedFile;
+
+  handleCameraOpen() async{
+    try {
+      final XFile? image = await ImagePicker().pickImage(source: ImageSource.camera);
+      setState(() {
+        pickedFile = image;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  handleGalleryOpen() async{
+    try {
+      final XFile? image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      setState(() {
+        pickedFile = image;
+      });
+    } catch (e) {
+      print(e);
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,18 +48,26 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(
-        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-        decoration: BoxDecoration(
-          color: Colors.green
-        ),
-        child: Text(
-          "Hello World",
-          style: TextStyle(
-            fontSize: 18,
-            color: Colors.black,
-            fontWeight: FontWeight.normal,
-            decoration: TextDecoration.none
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                child: Icon(Icons.image),
+                onPressed: handleGalleryOpen,
+              ),
+              ElevatedButton(
+                child: Icon(Icons.camera),
+                onPressed: handleCameraOpen,
+              ),
+              Container(
+                width: 200,
+                height: 200,
+                child: pickedFile != null ? Image.file(File(pickedFile!.path), fit: BoxFit.cover,): Container()
+              )
+            ]
           ),
         ),
       ),
